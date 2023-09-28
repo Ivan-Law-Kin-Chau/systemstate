881118d4ffab2772d0cdc07c628a29d9d3227b0d
Ivan Law Kin Chau <ivanlawkinchau@gmail.com>
2023-09-28 23:20:58 +0800

    Made it possible to select the search results from SearchBox

--- views/src/SearchBox.jsx
+++ views/src/SearchBox.jsx
@@ -9,7 +9,7 @@ import * as ReactAsync from "react-async";
 export const ModeContext = React.createContext();
 
 const InternalSearchBox = props => {
-	const [previousData, setPreviousData] = React.useState("Loading... ");
+	const [previousData, setPreviousData] = React.useState([]);
 	
 	const {data, error, isPending} = ReactAsync.useAsync({
 		promiseFn: window.search, 
@@ -17,35 +17,69 @@ const InternalSearchBox = props => {
 		watch: props.searchTerm
 	});
 	
-	if (isPending) return previousData;
+	let newPreviousData = previousData;
+	let hasChanged = false;
+	
+	// Make previousData identical to data
+	if (!isPending && !error && data) {
+		if (newPreviousData.length > data.length) {
+			newPreviousData.splice(data.length, newPreviousData.length - data.length);
+			hasChanged = true;
+		}
+		
+		for (let index = 0; index < data.length; index++) {
+			if (typeof data[index] === "string") break;
+			const stringified = JSON.stringify(data[index], null, "\t");
+			if (newPreviousData[index] !== stringified) {
+				newPreviousData[index] = stringified;
+				if (props.activeSearchResultIndex === index) props.setActiveSearchResultIndex(-1);
+				hasChanged = true;
+			}
+		}
+	}
+	
+	React.useEffect(() => {
+		if (hasChanged === true) {
+			setPreviousData(data);
+			props.setMaximumSearchResultIndex(data.length);
+		}
+	}, [data]);
+	
+	const activeRef = React.useRef(null);
+	React.useEffect(() => {
+		if (props.activeSearchResultIndex !== -1) {
+			activeRef.current.scrollIntoView({inline: "nearest", block: "nearest"});
+		}
+	}, [props.activeSearchResultIndex]);
 	
 	if (error) return "Something went wrong: " + error.message;
 	
-	if (data) {
-		const newData = JSON.stringify(data, null, "\t");
-		
-		if (previousData !== newData) {
-			setPreviousData(newData);
-		}
-		
-		return newData;
-	}
+	return <>{newPreviousData.map((datum, index) => props.activeSearchResultIndex === index ? <div key={index} ref={activeRef} style={{
+		border: "3px solid #000000"
+	}}>{datum}</div> : <div key={index} style={{
+		border: "3px solid #FFFFFF"
+	}}>{datum}</div>)}</>;
 }
 
 export const SearchBox = props => {
-	const inputRef = React.useRef(null);
-	React.useEffect(() => {
-		inputRef.current.focus();
-	}, []);
-	
 	const {editor, mode, setMode} = React.useContext(ModeContext);
 	
 	const [searchTerm, setSearchTerm] = React.useState("");
+	const [activeSearchResultIndex, setActiveSearchResultIndex] = React.useState(-1);
+	const [maximumSearchResultIndex, setMaximumSearchResultIndex] = React.useState(-1);
+	
+	const inputRef = React.useRef(null);
+	React.useEffect(() => {
+		if (activeSearchResultIndex === -1) {
+			inputRef.current.scrollIntoView({inline: "nearest", block: "nearest"});
+			inputRef.current.focus();
+		}
+	}, [activeSearchResultIndex]);
 	
 	return <pre style={{
 		margin: "0px", 
 		display: "inline"
-	}}>
+	}} onFocus={() => inputRef.current.focus()} onClick={() => inputRef.current.focus()}>
 		<span style={{
 			display: "none"
 		}} {...props.attributes}>{props.children}</span>
@@ -58,7 +92,18 @@ export const SearchBox = props => {
 				if (event.key === "Control") {
 					setMode("typing");
 					Transforms.removeNodes(editor, {at: [], match: (node, path) => node.isSearchBox === true});
+				} else if (event.key === "ArrowUp" && activeSearchResultIndex > -1) {
+					event.preventDefault();
+					setActiveSearchResultIndex(activeSearchResultIndex - 1);
+				} else if (event.key === "ArrowDown" && activeSearchResultIndex < maximumSearchResultIndex - 1) {
+					event.preventDefault();
+					setActiveSearchResultIndex(activeSearchResultIndex + 1);
 				}
+			}} style={{
+				outline: "none", 
+				position: "relative", 
+				zIndex: activeSearchResultIndex === -1 ? "2" : "auto", 
+				border: activeSearchResultIndex === -1 ? "3px solid #000000" : "3px solid #FFFFFF"
 			}}/>
 			
 			<span style={{
@@ -66,9 +111,9 @@ export const SearchBox = props => {
 				left: "0%", 
 				position: "absolute", 
 				zIndex: "1", 
-				backgroundColor: "rgba(255, 255, 255, 0.85)", 
+				backgroundColor: "#FFFFFF", 
 				border: "1px solid #000000"
-			}}><InternalSearchBox searchTerm={searchTerm}/></span>
+			}}><InternalSearchBox searchTerm={searchTerm} activeSearchResultIndex={activeSearchResultIndex} setActiveSearchResultIndex={setActiveSearchResultIndex} setMaximumSearchResultIndex={setMaximumSearchResultIndex}/></span>
 		</div>
 	</pre>;
 }
